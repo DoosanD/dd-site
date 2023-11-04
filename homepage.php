449cc89906ff070288ab1765483c8e9d78642742
DoosanD <davicdusan@gmail.com>
2023-11-04 18:50:18 +0100

    04-11-2023

---
+++ homepage.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Template Name: Homepage
+ * 
+ * The template for displaying Homepage Template
+ * 
+ * @package D_Theme
+ */
+
+get_header();
+?>
+
+<main id="primary" class="site-main">
+
+    <section class="home-section hero-section">
+        <h1>TESTING AREA</h1>
+    </section>
+
+</main><!-- #main -->
+
+<?php
+get_footer();
