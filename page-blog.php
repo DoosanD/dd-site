570927abe4ff22c84e891d700f96d546da99f0ba
DoosanD <davicdusan@gmail.com>
2024-03-31 20:57:12 +0200

    31-03-2024

---
+++ page-blog.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Template Name: Blog
+ * 
+ * The template for displaying Blog Template
+ * 
+ * @package D_Theme
+ */
+
+get_header();
+?>
+
+<main id="primary" class="site-main">
+
+    <section class="home-section hero-section">
+        <h1>Blog AREA</h1>
+    </section>
+
+</main><!-- #main -->
+
+<?php
+get_footer();
--- page-homepage.php
+++ page-homepage.php
@@ -14,7 +14,7 @@ get_header();
 <main id="primary" class="site-main">
 
     <section class="home-section hero-section">
-        <h1>TESTING AREA</h1>
+        <h1><?= CFS()->get('hero_title'); ?></h1>
     </section>
 
 </main><!-- #main -->
