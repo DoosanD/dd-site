eb609f625de68c6a2c4e29bab33af9d7d0a715ca
DoosanD <davicdusan@gmail.com>
2024-04-20 21:12:32 +0200

    20-04-2024

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,8 +13,9 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -21,33 +23,33 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<header id="masthead" class="site-header">
-		<div class="container section-container">
-			<div class="header-content">
-				<div class="site-branding">
-					<?php the_custom_logo(); ?>
-				</div><!-- .site-branding -->
+	<?php wp_body_open(); ?>
+	<div id="page" class="site">
+		<header id="masthead" class="site-header">
+			<div class="container section-container">
+				<div class="header-content">
+					<div class="site-branding --d-flex-center">
+						<?php the_custom_logo(); ?>
+					</div><!-- .site-branding -->
 
-				<nav id="site-navigation" class="d-navigation navbar-default navbar">
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle collapsed d-x-collapse" data-toggle="collapse" data-target="#d-collapse-menu">
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-					</div>
+					<nav id="site-navigation" class="d-navigation navbar-default navbar">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed d-x-collapse" data-toggle="collapse" data-target="#d-collapse-menu">
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						</div>
 
-					<div class="collapse navbar-collapse" id="d-collapse-menu">
-						<?php
-						wp_nav_menu( array(
-							'theme_location' => 'menu-1',
-							'menu_id'        => 'primary-menu',
-						) );
-						?>
-					</div>
-				</nav><!-- #site-navigation -->
+						<div class="collapse navbar-collapse" id="d-collapse-menu">
+							<?php
+							wp_nav_menu(array(
+								'theme_location' => 'menu-1',
+								'menu_id'        => 'primary-menu'
+							));
+							?>
+						</div>
+					</nav><!-- #site-navigation -->
+				</div>
 			</div>
-		</div>
-	</header><!-- #masthead -->
+		</header><!-- #masthead -->
