04ec265b7a49f94d2a72a320482138da1692c747
DoosanD <davicdusan@gmail.com>
2024-06-08 18:46:59 +0200

    08-06-2024

--- page-homepage.php
+++ page-homepage.php
@@ -13,10 +13,55 @@ get_header();
 
 <main id="primary" class="site-main">
 
+    <!-- <h1><?= CFS()->get('hero_title'); ?></h1> -->
     <section class="home-section hero-section">
-        <div class="container">
-            <h1><?= CFS()->get('hero_title'); ?></h1>
-        </div>
+        <?php
+        // Start the WordPress loop
+        $args = array(
+            'posts_per_page' => 1, // Only retrieve the latest post
+            'orderby' => 'date',
+            'order'   => 'DESC'
+        );
+
+        $query = new WP_Query($args);
+
+        if ($query->have_posts()) {
+            while ($query->have_posts()) {
+                $query->the_post(); ?>
+
+                <div class="container">
+                    <div class="col-md-6">
+                        <div class="hero-image-holder">
+                            <?php if (has_post_thumbnail()) : ?>
+                                <?php the_post_thumbnail('full', array('class' => 'hero-post-image')); ?>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                            <div class="entry-header">
+                                <h1 class="entry-title"><?php the_title(); ?></h1>
+                            </div>
+                            <div class="entry-content">
+                                <?php the_content(); ?>
+                            </div>
+                            <div class="entry-footer">
+                                <span class="posted-on"><?php the_date(); ?></span>
+                                <span class="byline"><?php the_author(); ?></span>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+
+        <?php }
+        } else {
+            echo '<p class="no-post-p">No posts found.</p>';
+        }
+
+        // Restore original post data
+        wp_reset_postdata();
+        ?>
+
     </section>
 
 </main><!-- #main -->
