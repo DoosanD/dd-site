ab32d5e6fb7a34af29c1c1c3c1857098e2be4cef
DoosanD <davicdusan@gmail.com>
2024-12-24 13:14:46 +0100

    24-12-2024

--- page-homepage.php
+++ page-homepage.php
@@ -14,7 +14,7 @@ get_header();
 <main id="primary" class="site-main">
 
     <!-- <h1><?= CFS()->get('hero_title'); ?></h1> -->
-    <!-- Iddle timer 23.12.2024 044 -->
+    <!-- Iddle timer 24.12.2024 045 -->
     <!-- HERO SECTION -->
     <section class="home-section hero-section">
         <?php
