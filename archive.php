5974c67460153bc6c665e6733bdb4b8be9440f96
DoosanD <davicdusan@gmail.com>
2026-02-03 09:51:10 +0100

    archive update

--- archive.php
+++ archive.php
@@ -8,44 +8,107 @@
  */
 
 get_header();
+$single_hero_backup = site_url() . '/wp-content/uploads/2024/04/Default_A_stunning_curvaceous_woman_with_cascading_locks_of_ha_3.jpg';
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
-		<?php if ( have_posts() ) : ?>
+	<!-- Hero Image Section -->
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+	<section class="post-hero">
+		<h1 class="page-hero-title">
+			<?php the_archive_title(); ?>
+		</h1>
+		<img class="single-hero-img" src="<?php echo $single_hero_backup; ?>" alt="Hero Image">
+	</section>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+	<!-- Blog Posts Section -->
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+	<section class="blog-posts-section">
+		<div class="container">
+			<div class="blog-posts-holder">
+				<div class="blog-posts-main col-md-8">
+					<?php
+					if (have_posts()):
+						while (have_posts()):
+							the_post();
+							?>
+							<article id="post-<?php the_ID(); ?>" <?php post_class('blog-post'); ?>>
+								<?php
+								$fallback_image_id = 71;
+								if (has_post_thumbnail()):
+									?>
+									<div class="blog-post-thumbnail">
+										<a href="<?php the_permalink(); ?>">
+											<?php the_post_thumbnail('full', ['alt' => get_the_title(), 'class' => 'blog-post-thumbnail-img']); ?>
+										</a>
+									</div>
+								<?php elseif ($fallback_image_id):
+									$fallback_image = wp_get_attachment_image($fallback_image_id, 'full', false, ['alt' => get_the_title(), 'class' => 'blog-post-thumbnail-img']);
+									if ($fallback_image):
+										?>
+										<div class="blog-post-thumbnail">
+											<a href="<?php the_permalink(); ?>">
+												<?php echo $fallback_image; ?>
+											</a>
+										</div>
+										<?php
+									endif;
+								endif;
+								?>
+								<div class="blog-post-information">
+									<h2 class="blog-post-title">
+										<a href="<?php the_permalink(); ?>" class="blog-post-link">
+											<?php the_title(); ?>
+										</a>
+									</h2>
+									<div class="blog-post-meta">
+										<span class="blog-post-date">
+											<?php echo get_the_date(); ?>
+										</span>
+										<span class="blog-post-author">by
+											<?php the_author(); ?>
+										</span>
+									</div>
+									<div class="blog-post-excerpt">
+										<?php the_excerpt(); ?>
+									</div>
+								</div>
+							</article>
+							<?php
+						endwhile;
 
-			endwhile;
+						// Add pagination links here
+						global $wp_query;
+						?>
+						<div class="pagination">
+							<?php
+							echo paginate_links(array(
+								'total' => $wp_query->max_num_pages,
+								'current' => max(1, get_query_var('paged')),
+								'prev_text' => __('« Previous', 'd-theme'),
+								'next_text' => __('Next »', 'd-theme'),
+							));
+							?>
+						</div>
+						<?php
+					else:
+						?>
+						<p>
+							<?php esc_html_e('No posts found.', 'd-theme'); ?>
+						</p>
+						<?php
+					endif;
+					?>
+				</div>
+				<div class="blog-posts-sidebar col-md-4">
+					<?php get_template_part('template-parts/template-part', 'sidebar'); ?>
+				</div>
+			</div>
+		</div>
+	</section>
 
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
+</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
